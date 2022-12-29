521c3cc5a299bc2049d66ad27647045a699ee88c
Sergey <sergey.sav.26666@mail.ru>
2022-12-29 22:52:10 +0300

    начало работы с сайтом

---
+++ chat/src/pages/Main.jsx
@@ -0,0 +1,13 @@
+function Chat() {
+    return (
+        <div className='chat'>
+            <div className='chat__wrapper'>
+                <div className='chat__nav'></div>
+                <div className='chat__communion'></div>
+                <div className='chat__input'></div>
+            </div>
+        </div>
+    );
+}
+
+export default Chat;
